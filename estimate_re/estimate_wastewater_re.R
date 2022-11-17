58c5f36ff3bce30a4a93caf13b98eb2eed9821e0
Sarah Nadeau <san62@cornell.edu>
2022-11-17 12:04:03 +0100

    Set up github action workflow to calculate Re

---
+++ estimate_re/estimate_wastewater_re.R
@@ -0,0 +1,170 @@
+# This script is to estimate Re from WW influenza concentrations.
+# It's based on JH's script https://github.com/JSHuisman/wastewaterRe/blob/main/code/wastewater_Zurich.R.
+
+# Load dependencies
+library(dplyr)
+library(tidyr)
+library(estimateR)  # https://github.com/covid-19-Re/estimateR
+
+source("estimate_re/helper_scripts/functions.R")
+source("estimate_re/helper_scripts/parameters.R")
+
+# Set variables
+delay_dist_info <- influenza_distribution_infection_to_shedding_fecal_moments
+mean_serial_interval <- influenza_mean_serial_interval_days
+std_serial_interval <- influenza_std_serial_interval_days
+estimation_window <- 3  # 3 is EpiEstim default
+n_bootstrap_reps <- 100  # TODO: increase?
+
+# Import data
+ww_data_bs <- read_csv("estimate_re/data/clean_data_bs.csv", col_types = cols(sample_date = "D")) %>%
+  pivot_wider(
+    id_cols = c("sample_date", "wwtp", "n_measurements"), 
+    names_from = "measurement_type", 
+    values_from = "mean")
+ww_data_ge <- read_csv("estimate_re/data/clean_data_ge_zh.csv", col_types = cols(sample_date = "D")) %>%
+  filter(wwtp == "STEP Aire") %>%
+  pivot_wider(
+    id_cols = c("sample_date", "wwtp", "n_measurements"), 
+    names_from = "measurement_type", 
+    values_from = "mean")
+ww_data_zh <- read_csv("estimate_re/data/clean_data_ge_zh.csv", col_types = cols(sample_date = "D")) %>%
+  filter(wwtp == "ARA Werdhölzli") %>%
+  pivot_wider(
+    id_cols = c("sample_date", "wwtp", "n_measurements"), 
+    names_from = "measurement_type", 
+    values_from = "mean")
+
+# Normalize daily copies by minimum value to get observations on the same scale as case data
+# See Huisman et al. 2022 Environmental Health Perspectives - deconvolution algorithm seems to struggle when observations are too peaked
+# TODO: using lowest non-zero measurement - any other strategy to consider?
+normalization_factor_bs <- min(
+  ww_data_bs$IAV_gc_per_day[ww_data_bs$IAV_gc_per_day > 0],
+  ww_data_bs$IBV_gc_per_day[ww_data_bs$IBV_gc_per_day > 0]
+)
+normalization_factor_ge <- min(
+  ww_data_ge$IAV_gc_per_day[ww_data_ge$IAV_gc_per_day > 0],
+  ww_data_ge$IBV_gc_per_day[ww_data_ge$IBV_gc_per_day > 0]
+)
+normalization_factor_zh <- min(
+  ww_data_zh$IAV_gc_per_day[ww_data_zh$IAV_gc_per_day > 0],
+  ww_data_zh$IBV_gc_per_day[ww_data_zh$IBV_gc_per_day > 0]
+)
+ww_data_bs <- ww_data_bs %>%
+  mutate(IAV_gc_per_day_norm = IAV_gc_per_day / normalization_factor_bs) %>%
+  mutate(IBV_gc_per_day_norm = IBV_gc_per_day / normalization_factor_bs)
+ww_data_ge <- ww_data_ge %>%
+  mutate(IAV_gc_per_day_norm = IAV_gc_per_day / normalization_factor_ge) %>%
+  mutate(IBV_gc_per_day_norm = IBV_gc_per_day / normalization_factor_ge)
+ww_data_zh <- ww_data_zh %>%
+  mutate(IAV_gc_per_day_norm = IAV_gc_per_day / normalization_factor_zh) %>%
+  mutate(IBV_gc_per_day_norm = IBV_gc_per_day / normalization_factor_zh)
+
+# Interpolate measurements to daily values
+ww_data_interp_bs <- interpolate_measurements(
+  data_frame = ww_data_bs,
+  date_col = "sample_date",
+  measurement_cols = c("IAV_gc_per_day", "IAV_gc_per_mL_WW", "IBV_gc_per_day", "IBV_gc_per_mL_WW", "IAV_gc_per_day_norm", "IBV_gc_per_day_norm")
+)
+ww_data_interp_ge <- interpolate_measurements(
+  data_frame = ww_data_ge,
+  date_col = "sample_date",
+  measurement_cols = c("IAV_gc_per_day", "IAV_gc_per_mL_WW", "IBV_gc_per_day", "IBV_gc_per_mL_WW", "IAV_gc_per_day_norm", "IBV_gc_per_day_norm")
+)
+ww_data_interp_zh <- interpolate_measurements(
+  data_frame = ww_data_zh,
+  date_col = "sample_date",
+  measurement_cols = c("IAV_gc_per_day", "IAV_gc_per_mL_WW", "IBV_gc_per_day", "IBV_gc_per_mL_WW", "IAV_gc_per_day_norm", "IBV_gc_per_day_norm")
+)
+
+# Put data in long format
+data_all <- rbind(
+  ww_data_interp_bs %>% mutate(wwtp = "ARA Basel"),
+  ww_data_interp_ge %>% mutate(wwtp = "STEP Aire"),
+  ww_data_interp_zh %>% mutate(wwtp = "ARA Werdhölzli")
+) %>% pivot_longer(
+  cols = c(IAV_gc_per_day, IBV_gc_per_day),
+  values_to = "genome_copies_per_day",
+  names_to = "influenza_type"
+) %>% mutate(
+  influenza_type = recode(influenza_type, IAV_gc_per_day = "IAV", IBV_gc_per_day = "IBV")
+) %>% select(sample_date, wwtp, n_measurements, is_observation, influenza_type, genome_copies_per_day)
+
+# Write out data used for Re inference
+write.csv(x = data_all, file = "app/data/ww_loads.csv")
+
+# Estimate Re for each data stream
+is_first <- T
+for (wwtp_i in unique(data_all$wwtp)) {
+  for (influenza_type_j in unique(data_all$influenza_type)) {
+    writeLines(paste("\nEstimating Re for", wwtp_i, "influenza", influenza_type_j))
+    
+    # Get appropriate data
+    data_filtered <- data_all %>%
+      filter(wwtp == wwtp_i, influenza_type == influenza_type_j) %>%
+      arrange(sample_date)
+    
+    measurements = list(
+      values = data_filtered$genome_copies_per_day,
+      index_offset = 0)
+    
+    # Try to estimate Re (handling case where not enough incidence observed to calculate)
+    estimates_bootstrap <- tryCatch({
+      get_block_bootstrapped_estimate(
+        measurements$values,
+        N_bootstrap_replicates = n_bootstrap_reps,
+        smoothing_method = "LOESS",
+        deconvolution_method = "Richardson-Lucy delay distribution",
+        estimation_method = "EpiEstim sliding window",
+        uncertainty_summary_method = "original estimate - CI from bootstrap estimates",
+        combine_bootstrap_and_estimation_uncertainties = TRUE,
+        delay = delay_dist_info,
+        estimation_window = estimation_window,
+        mean_serial_interval = mean_serial_interval,
+        std_serial_interval = std_serial_interval,
+        ref_date = min(data_filtered$sample_date),
+        time_step = "day",
+        output_Re_only = F) %>% 
+        mutate(observation_type = wwtp_i, influenza_type = influenza_type_j)
+    },
+    error = function(cond) {
+      message(paste("Couldn't calculate Re for", wwtp_i, "influenza", influenza_type_j))
+      message(cond)
+      # Make observation data frame anyways
+      return(data.frame(
+        date = data_filtered$sample_date,
+        observed_incidence = data_filtered$genome_copies_per_day,
+        CI_down_observed_incidence = NA,    
+        CI_up_observed_incidence = NA,
+        smoothed_incidence = NA,
+        CI_down_smoothed_incidence = NA,
+        CI_up_smoothed_incidence = NA,
+        deconvolved_incidence = NA,
+        CI_down_deconvolved_incidence = NA,
+        CI_up_deconvolved_incidence = NA,
+        Re_estimate = NA,
+        CI_down_Re_estimate = NA,
+        CI_up_Re_estimate = NA,
+        Re_highHPD = NA,
+        Re_lowHPD = NA,
+        bootstrapped_CI_down_Re_estimate = NA,
+        bootstrapped_CI_up_Re_estimate = NA,
+        observation_type = wwtp_i,
+        influenza_type = influenza_type_j
+      ))
+    })
+    
+    # Aggregate Re estimates
+    if (is_first) {
+      estimates_bootstrap_all <- estimates_bootstrap
+      is_first <- F
+    } else {
+      estimates_bootstrap_all <- rbind(estimates_bootstrap_all, estimates_bootstrap)
+    } 
+  }
+}
+
+write.csv(
+  x = estimates_bootstrap_all,
+  file = "app/data/ww_re_estimates.csv"
+)
---
+++ estimate_re/helper_scripts/parameters.R
@@ -0,0 +1,57 @@
+# This script stores parameters for different parameterizations of wastewater Re estimation, depending on the pathogen
+
+## SARS-CoV-2  Delay between infection and onset of symptoms (incubation period) in days
+# Ref: Linton et al., Journal of Clinical Medicine, 2020
+sars_cov_2_distribution_incubation <- list(
+    name = "gamma", 
+    shape = 3.2, 
+    scale = 1.3)
+
+
+## SARS-CoV-2 Delay between onset of symptoms and case confirmation in days
+# Ref: Bi et al., The Lancet Infectious Diseases, 2020
+sars_cov_2_distribution_onset_to_confirmation <- list(
+    name = "gamma", 
+    shape = 2.1,
+    scale = 2.6)
+
+## SARS-CoV-2 Delay between onset of symptoms and shedding into wastewater in days
+# Ref: Benefield et al., medRxiv, 2020
+sars_cov_2_distribution_onset_to_shedding <- list(
+    name = "gamma", 
+    shape = 0.929639, 
+    scale = 7.241397)
+
+## SARS-CoV-2 Serial interval (for Re estimation) in days
+# Ref: Nishiura et al.,International Journal of Infectious Diseases, 2020
+sars_cov_2_mean_serial_interval_days <- 4.8
+sars_cov_2_std_serial_interval_days <- 2.3
+
+## Influenza Delay between infection and shedding into wastewater in days
+# Ref: Carrat et al., American Journal of Epidemiology, 2008
+influenza_distribution_infection_to_shedding_carrat2008_mle <- list(
+    name = "gamma",
+    shape = 8.63,
+    scale = 0.29)
+
+influenza_distribution_infection_to_shedding_carrat2008_moments <- list(
+    name = "gamma",
+    shape = 6.153341704,
+    scale = 0.404855929)
+
+## Influenza Delay between infection and shedding into wastewater in days
+# Ref: get_shedding_load_distribution_fecal.R
+influenza_distribution_infection_to_shedding_fecal_moments <- list(
+    name = "gamma",
+    shape = 1.769032,
+    scale = 5.74016)
+
+## Influenza Delay between infection and symptom score in weeks
+# Ref: get_symptom_score_distribution.R
+influenza_distribution_infection_to_symptoms_moments <- list(
+  name = "gamma",
+  shape = 4.469965356,
+  scale = 0.117523934)
+
+influenza_mean_serial_interval_days <- 2.6  # EpiEstim example (from Ferguson et al. 2005, Nature)
+influenza_std_serial_interval_days <- 1.5  # EpiEstim example (from Ferguson et al. 2005, Nature)
