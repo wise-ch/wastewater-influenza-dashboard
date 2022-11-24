002ceeedb79c1f46450ed3df236c28749ba40041
Adrian Lison <adrian.lison@bsse.ethz.ch>
2022-11-24 19:08:15 +0100

    Actually use newest_data_file

--- R/helper_scripts/preprocess_data_cases_che.R
+++ R/helper_scripts/preprocess_data_cases_che.R
@@ -10,7 +10,7 @@ get_newest_data <- function(path_to_data = "data/raw_data/foph_case_data") {
   files <- list.files(path = path_to_data, pattern = ".*Influenza_daten_nach_plz.xlsx$", full.names = T)
   newest_data_file <- sort(files)[length(files)]
   print(paste("Newest file found is:", newest_data_file))
-  newest_data <- readxl::read_xlsx("data/raw_data/foph_case_data/2022-11-15_Influenza_daten_nach_plz.xlsx", sheet = 1) %>% 
+  newest_data <- readxl::read_xlsx(newest_data_file, sheet = 1) %>% 
     dplyr::rename_all(~make.names(.))
   return(newest_data)
 }
