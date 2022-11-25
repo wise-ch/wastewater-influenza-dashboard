5c670c5123160ed70e17e085695cf05c0aa4bd10
Sarah Nadeau <san62@cornell.edu>
2022-11-25 17:47:25 +0100

    add all-catchment comparison to app, first-try include case data

--- app/helper_code/make_plots.R
+++ app/helper_code/make_plots.R
@@ -20,7 +20,7 @@ ww_loads <- ww_loads %>%
 
 # Join Re data
 re_to_plot <- bind_rows(
-  case_re_estimates %>% 
+  case_re_estimates %>%
     mutate(wwtp = recode(
       observation_type,
       "BASEL" = "ARA Basel",
@@ -47,13 +47,15 @@ re_to_plot <- bind_rows(
 )
 
 # Data type colors
-data_type_colors <- RColorBrewer::brewer.pal(name = "Set2", n = 3)[1:2]
+data_type_colors <- brewer.pal(n = 8, name = "Set1")[1:2]
 names(data_type_colors) <- c("Confirmed cases", "Wastewater")
 
 # WWTP colors
 wwtp_colors <- RColorBrewer::brewer.pal(name = "Set2", n = 7)
-wwtp_labels <- c("ARA Werhölzli Zurich", "STEP Aire Geneva", "ARA Basel", "IDA CDA Lugano", "ARA Chur", "ARA Sensetal Laupen", "ARA Altenrhein")
-wwtp_breaks <- wwtp_labels
+wwtp_levels <- c("ARA Werhölzli Zurich", "STEP Aire Geneva", "ARA Basel", "IDA CDA Lugano", "ARA Chur", "ARA Sensetal Laupen", "ARA Altenrhein", "All provided data")
+wwtp_labels <- c("Zurich", "Geneva", "Basel", "Lugano", "Chur", "Laupen", "Altenrhein", "Cases all\ncatchments")
+
+re_to_plot <- re_to_plot %>% mutate(wwtp_factor = factor(wwtp, levels = wwtp_levels, labels = wwtp_labels))
 
 # Define shared theme for plots
 shared_theme <- theme_minimal() +
@@ -73,8 +75,10 @@ date_range <- range(ww_loads$sample_date)
 #' Plot wastewater loads
 plot_ww_loads <- function(data = ww_loads, wwtp_to_plot, date_range) {
   data_filtered <- data %>%
-    filter(is_observation, observation_units == "gc_per_day_norm") %>%
-    mutate(observation = observation) %>%
+    mutate(
+      wwtp = zoo::na.locf(wwtp),
+      influenza_type = zoo::na.locf(influenza_type),
+      measuring_period = zoo::na.locf(measuring_period)) %>%  # fill gaps due to interpolation
     filter(wwtp == wwtp_to_plot) %>%
     filter(sample_date >= date_range[1] & sample_date <= date_range[2])
 
@@ -84,8 +88,9 @@ plot_ww_loads <- function(data = ww_loads, wwtp_to_plot, date_range) {
       aes(x = sample_date, y = observation), color = data_type_colors["Wastewater"]
     ) +
     geom_line(
-      data = data_filtered %>% filter(is_observation),
-      aes(x = sample_date, y = observation, group = measuring_period), linetype = "dashed", colour = "black"
+      data = data_filtered,
+      aes(x = sample_date, y = observation, group = measuring_period),
+      linetype = "dashed", colour = "black"
     ) +
     facet_grid(. ~ influenza_type) +
     scale_x_date(
@@ -93,7 +98,26 @@ plot_ww_loads <- function(data = ww_loads, wwtp_to_plot, date_range) {
       date_breaks = "months", date_labels = "%b"
     ) +
     scale_y_continuous(labels = function(label) sprintf("%4.1f", label)) +
-    labs(x = "Date", y = "Genome copies per day (normalized by minimum value)") +
+    labs(x = "Date", y = "Genome copies per day\n(normalized by minimum value)") +
+    shared_theme
+
+  p
+}
+
+plot_cases <- function(data = re_to_plot, wwtp_to_plot, date_range) {
+  data_filtered <- data %>%
+    filter(wwtp == wwtp_to_plot, data_type == "Confirmed cases") %>%
+    filter(date >= date_range[1] & date <= date_range[2])
+
+  p <- ggplot(data = data_filtered, aes(x = date, y = observed_incidence)) +
+    geom_line(aes(color = "Confirmed cases", group = measuring_period), lwd = 1) +
+    facet_grid(. ~ influenza_type) +
+    scale_x_date(
+      limits = c(date_range[1], date_range[2]),
+      date_breaks = "months", date_labels = "%b"
+    ) +
+    scale_y_continuous(labels = function(label) sprintf("%4.1f", label)) +
+    labs(x = "Date", y = "Confirmed cases\nin the catchment") +
     shared_theme
 
   p
@@ -129,52 +153,31 @@ plot_re <- function(data = re_to_plot, data_types, wwtp_to_plot, date_range) {
   p
 }
 
-# # Plotting for all plants --------------
-# 
-# canton_plotter <- function(source, canton, pathogen, date_range, i18n = NA) {
-#   data_filtered <- plotDataRe %>%
-#     filter(region %in% canton) %>%
-#     filter(pathogen_type == pathogen) %>%
-#     filter(data_type %in% source) %>%
-#     filter(date >= date_range[1] & date <= date_range[2])
-# 
-#   ylimits <- c(0, 2) # TODO: re-implement reactive y limits
-# 
-#   p <- ggplot(data = data_filtered) +
-#     geom_line(aes(
-#       x = date, y = median_R_mean, colour = region,
-#       alpha = protocol
-#     ), lwd = 0.8) +
-#     geom_ribbon(aes(
-#       x = date, ymin = median_R_lowHPD,
-#       ymax = median_R_highHPD, fill = region,
-#       alpha = protocol
-#     )) +
-#     geom_hline(yintercept = 1) +
-#     scale_color_manual(
-#       values = canton_colors,
-#       labels = canton_labels,
-#       breaks = canton_breaks,
-#       aesthetics = c("color", "fill")
-#     ) +
-#     scale_alpha_manual(values = protocol_alphas - 0.5, na.value = 0.2) + # reduce alpha for busier plots
-#     scale_x_date(
-#       limits = c(date_range[1], date_range[2]),
-#       date_breaks = "months", date_labels = "%b"
-#     ) +
-#     coord_cartesian(ylim = ylimits) +
-#     labs(
-#       x = i18n$t("Date"), y = get_re_label(i18n),
-#       fill = i18n$t("Catchment area"), alpha = "Protocol"
-#     ) +
-#     theme_minimal() +
-#     shared_theme +
-#     guides(color = "none")
-# 
-#   # Remove alpha legend unless protocol changed
-#   if (length(unique(data_filtered$protocol)) == 1) {
-#     p <- p + guides(alpha = "none")
-#   }
-# 
-#   p
-# }
+# Plot Re for all catchments together
+plot_all_re <- function(data = re_to_plot, data_types, date_range) {
+  data_filtered <- data %>%
+    filter(data_type %in% data_types) %>%
+    filter(date >= date_range[1] & date <= date_range[2])
+
+  ylimits <- c(0, 2) # TODO: re-implement reactive y limits
+
+  p <- ggplot(data = data_filtered) +
+    geom_line(aes(x = date, y = Re_estimate, colour = data_type), lwd = 0.8) +
+    geom_ribbon(aes(x = date, ymin = CI_down_Re_estimate, ymax = CI_up_Re_estimate, fill = data_type), alpha = 0.5) +
+    facet_grid(wwtp_factor ~ influenza_type, drop = F) +
+    geom_hline(yintercept = 1) +
+    scale_color_manual(values = data_type_colors, aesthetics = c("color", "fill"), breaks = data_types) +
+    scale_x_date(
+      limits = c(date_range[1], date_range[2]),
+      date_breaks = "months", date_labels = "%b"
+    ) +
+    scale_y_continuous(labels = function(label) sprintf("%6.1f", label)) +
+    coord_cartesian(ylim = ylimits) + # change this? Autoadjust? but how?
+    labs(
+      x = "Date", y = "Reproductive number",
+      colour = "Data type", fill = "Data type"
+    ) +
+    shared_theme
+
+  p
+}
--- app/ui.R
+++ app/ui.R
@@ -6,23 +6,16 @@ library(shinyWidgets)
 
 source("helper_code/make_plots.R")
 
-navbarPageWithInputs <- function(..., inputs) {
-  navbar <- navbarPage(...)
-  form <- tags$form(class = "navbar-form navbar-right", inputs)
-  navbar[[3]][[1]]$children[[1]] <- htmltools::tagAppendChild(
-    navbar[[3]][[1]]$children[[1]], form
-  )
-  navbar
-}
+navbarPage(
+  title = "Wastewater Re",
+  selected = "Single catchment",
 
-navbarPageWithInputs("Wastewater Re",
-  # a page with a navigation bar
-  # HOME ####
   tabPanel(
-    title = uiOutput("title_panel"),
+    title = "Single catchment",
 
-    # Sidepanel - options + info  --------
     sidebarLayout(
+
+      # Sidepanel - options + info  --------
       sidebarPanel(
         selectInput(
           inputId = "wwtp",
@@ -37,9 +30,12 @@ navbarPageWithInputs("Wastewater Re",
             "Geneva" = "STEP Aire Geneva"
           ),
         ),
-        shinyjs::useShinyjs(),
-        uiOutput("data_type"),
-        uiOutput("disabled_data_types"),
+        checkboxGroupInput(
+          inputId = "data_type",
+          label = "Data source (select to compare):",
+          choices = c("Wastewater", "Confirmed cases"),
+          selected = "Wastewater"
+        ),
         width = 3
       ),
 
@@ -49,20 +45,23 @@ navbarPageWithInputs("Wastewater Re",
           div(
             style = "position:relative",
             plotOutput("raw_plots",
-              height = "250px", width = "950px",
-              hover = hoverOpts("plot_hover_raw", delay = 10)
+              height = "250px", width = "950px"
             ) %>%
-              withSpinner(color = "#0dc5c1"),
-            uiOutput("hover_info_raw")
+              withSpinner(color = "#0dc5c1")
+          ),
+          div(
+            style = "position:relative",
+            plotOutput("case_plots",
+                       height = "250px", width = "950px"
+            ) %>%
+              withSpinner(color = "#0dc5c1")
           ),
           div(
             style = "position:relative",
             plotOutput("re_plots",
-              height = "255px", width = "950px",
-              hover = hoverOpts("plot_hover_re", delay = 10)
+              height = "255px", width = "950px"
             ) %>%
-              withSpinner(color = "#0dc5c1"),
-            uiOutput("hover_info_re")
+              withSpinner(color = "#0dc5c1")
           ),
           # slider input for date range -----
           shinyWidgets::setSliderColor(color = rep("darkgrey", 2), 1:2),
@@ -80,67 +79,44 @@ navbarPageWithInputs("Wastewater Re",
       ) # main panel
     ) # Sidebar layout
   ), # Home panel
-  
-  # # Switzerland  - rww comparison ----------
-  # tabPanel(
-  #   i18n$t("Switzerland"),
-  #   sidebarLayout(
-  #     sidebarPanel(
-  #       selectInput(
-  #         inputId = "pathogen_switzerland",
-  #         label = i18n$t("Select pathogen:"),
-  #         choices = c(
-  #           "SARS-CoV-2" = "COVID",
-  #           "Influenza A Virus" = "IAV",
-  #           "Influenza B Virus" = "IBV"
-  #         ),
-  #         selected = "COVID"
-  #       ),
-  #       shinyjs::useShinyjs(),
-  #       uiOutput("region_switzerland"),
-  #       uiOutput("region_switzerland_disabled"),
-  #       width = 3
-  #     ),
-  #     mainPanel(
-  #       fluidRow(
-  #         div(
-  #           style = "position:relative",
-  #           plotOutput("rww_plots",
-  #             height = "350px", width = "980px",
-  #             hover = hoverOpts("plot_hover_rww", delay = 10)
-  #           ) %>%
-  #             withSpinner(color = "#0dc5c1"),
-  #           uiOutput("hover_info_rww")
-  #         ),
-  #         div(
-  #           style = "position:relative",
-  #           plotOutput("rcc_plots",
-  #             height = "350px", width = "980px",
-  #             hover = hoverOpts("plot_hover_rcc", delay = 10)
-  #           ) %>%
-  #             withSpinner(color = "#0dc5c1"),
-  #           uiOutput("hover_info_rcc")
-  #         ),
-  #         # shinyWidgets::setSliderColor(color = "#5dc863FF", 1:2), # for some reason, this is not working?
-  #         # shinyWidgets::chooseSliderSkin("Flat", color = "#5dc863FF"),
-  #         sliderInput("slider_dates_cantonal",
-  #           label = NULL, width = "950px",
-  #           min = as.Date("2021-02-01"), max = Sys.Date(),
-  #           value = c(as.Date("2021-02-01"), Sys.Date())
-  #         ),
-  #         p(HTML(paste0("<em>", i18n$t("(The start and end date of the time interval to be displayed can be changed by moving the slider above.)"), "</em>")),
-  #           style = "margin-bottom:0;font-size: 90%;"
-  #         ),
-  #         p(i18n$t("The raw measurements of SARS-CoV-2 in wastewater and associated catchment cases are displayed on "),
-  #           a(href = "https://sensors-eawag.ch/sars/overview.html", i18n$t("EAWAG's overview page"), .noWS = "outside"),
-  #           i18n$t(" with links to individual plant measurements."),
-  #           .noWS = c("after-begin", "before-end")
-  #         )
-  #       ) # fluid row
-  #     )
-  #   )
-  # ),
-  
+
+  # All-catchment comparison ----------
+  tabPanel(
+    "All catchments",
+
+    sidebarLayout(
+      sidebarPanel(
+        checkboxGroupInput(
+          inputId = "data_type_all_catchments",
+          label = "Data source (select to compare):",
+          choices = c("Wastewater", "Confirmed cases"),
+          selected = "Wastewater"
+        ),
+        width = 3
+      ),
+
+      mainPanel(
+        fluidRow(
+          div(
+            style = "position:relative",
+            plotOutput("all_catchment_plots",
+              height = "1050px", width = "980px"
+            ) %>%
+              withSpinner(color = "#0dc5c1")
+          ),
+          sliderInput("slider_dates_cantonal",
+            label = NULL, width = "950px",
+            min = as.Date("2021-02-01"), max = Sys.Date(),
+            value = c(as.Date("2021-02-01"), Sys.Date())
+          ),
+          p(HTML(paste0("<em>", "(The start and end date of the time interval to be displayed can be changed by moving the slider above.)", "</em>")),
+            style = "margin-bottom:0;font-size: 90%;"
+          )
+        ) # fluid row
+      )  # main panel
+    )  # Sidebar layout
+  ),
+
   # About ---------
   tabPanel(
     "About",
@@ -151,12 +127,5 @@ navbarPageWithInputs("Wastewater Re",
       ),
       hr()
     )
-  ),
-  inputs = selectInput(inputId = "lang", label = NULL,
-                       choices = c("EN" = "en-gb",
-                                   "DE" = "de-ch", 
-                                   "FR" = "fr-ch", 
-                                   "IT" = "it-ch"
-                       ), width = '80px')
-  
+  )
 )
