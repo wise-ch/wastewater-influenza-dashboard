f2cebbe1643285669637b65f10b279510164c800
SarahNadeau <san62@cornell.edu>
2022-12-05 11:18:48 +0100

    implement rest of OL et al. suggestions

--- app/ui.R
+++ app/ui.R
@@ -32,18 +32,18 @@ navbarPage(
           selected = "ARA Werdhölzli Zurich"
         ),
         checkboxGroupInput(
+          inputId = "measuring_periods",
+          label = "Influenza season (select to compare):",
+          choices = c("2021/22", "2022/23"),
+          selected = c("2021/22", "2022/23")
+        ),
+        checkboxGroupInput(
           inputId = "data_type",
           label = "Data source (select to compare):",
           choices = c(
             "Virus load in wastewater" = "Wastewater",
             "Laboratory-confirmed cases" = "Confirmed cases"),
           selected = "Wastewater"
-        ),
-        checkboxGroupInput(
-          inputId = "measuring_periods",
-          label = "Influenza season (select to compare):",
-          choices = c("2021/22", "2022/23"),
-          selected = c("2021/22", "2022/23")
         ),
         width = 3
       ),
@@ -74,6 +74,9 @@ navbarPage(
           ),
           htmlOutput("link"),
           downloadButton('download_plot', 'Download plots')
+        ),
+        p("*Note that catchment area refers to the intake area for each wastewater treatment plant. Per-catchment laboratory-confirmed cases are estimated based on the number of cases per postal code and a delineation of each catchment area. This estimation can result in non-integer case numbers. For more informaton on the specific catchments studied, see the ",
+                   a(href = "https://www.eawag.ch/en/department/sww/projects/sars-cov2-in-wastewater/", "EAWAG project page for SARS-CoV-2 monitoring in wastewater", .noWS = "outside")
         ) # fluid row
       ) # main panel
     ) # Sidebar layout
