ae2e08e227d2972b44f1ebec2e850d42e2d9ba63
Adrian Lison <adrian.lison@bsse.ethz.ch>
2023-03-06 13:22:40 +0100

    Add automatic preprocessing script

---
+++ preprocess_data.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+Rscript "R/helper_scripts/preprocess_data_eawag.R"
+Rscript "R/helper_scripts/preprocess_data_cases_che.R"
