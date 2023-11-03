2f7c601c30d642dd9ccb869ad443e00d5058965d
Adrian Lison <adrian.lison@bsse.ethz.ch>
2023-11-03 14:09:43 +0100

    Update preprocessing to new resp6 assay

--- R/helper_scripts/preprocess_data_eawag.R
+++ R/helper_scripts/preprocess_data_eawag.R
@@ -7,27 +7,94 @@ library(dplyr)
 source("R/local_config.R")
 source("R/helper_scripts/utils_preprocess.R")
 
-print("Loading newest data from EAWAG")
+## Old data from RESP4 pannel
+print("Loading newest RESP4 data from EAWAG")
 file_eawag <- get_newest_file(dir = "data/raw_data/eawag_data", filename_pattern = "^LatestFluData.*csv$")
-data_eawag <- read.csv(file_eawag, fileEncoding = "UTF-8") %>%
+data_eawag4 <- read.csv(file_eawag, fileEncoding = "UTF-8") %>%
   mutate(sample_date = as.Date(sample_date),
          wwtp = ifelse(grepl(wwtp, pattern = "ARA Werdh"), "ARA Werdhölzli", wwtp))
-
 
 eawag_cloud_filepath <- file.path(eawag_cloud_folder,"LatestFluData.csv")
 newest_data_cloud <- read.csv(eawag_cloud_filepath, fileEncoding = "UTF-8") %>%
   mutate(sample_date = as.Date(sample_date),
          wwtp = ifelse(grepl(wwtp, pattern = "ARA Werdh"), "ARA Werdhölzli", wwtp))
 
-if (!identical(data_eawag, newest_data_cloud)) {
-  message("Copying new data from cloud folder to local folder")
+if (!identical(data_eawag4, newest_data_cloud)) {
+  message("Copying new RESP4 data from cloud folder to local folder")
   date_cloudfile_modified <- format(as.Date(file.info(eawag_cloud_filepath)$mtime)+1, "%y-%m-%d")
   file.copy(eawag_cloud_filepath, file.path("data/raw_data/eawag_data",paste0("LatestFluData_",date_cloudfile_modified,".csv")))
-  data_eawag <- newest_data_cloud
+  data_eawag4 <- newest_data_cloud
 } else {
-  message("No new data in cloud folder found.")
+  message("No new RESP4 data in cloud folder found.")
 }
 
+## New data from RESP6 pannel
+print("Loading newest RESP6 data from EAWAG")
+file_eawag6 <- get_newest_file(dir = "data/raw_data/eawag_data", filename_pattern = "^FilteredData-RESP6.*csv$")
+data_eawag6 <- read.csv(file_eawag6, fileEncoding = "UTF-8") %>%
+  mutate(sample_date = as.Date(SampleDate),
+         wwtp = ifelse(grepl(wwtp_name, pattern = "ARA Werdh"), "ARA Werdhölzli", wwtp_name))
+
+eawag_cloud_filepath6 <- file.path(eawag_cloud_folder,"FilteredData-RESP6.csv")
+newest_data_cloud6 <- read.csv(eawag_cloud_filepath6, fileEncoding = "UTF-8") %>%
+  mutate(sample_date = as.Date(SampleDate),
+         wwtp = ifelse(grepl(wwtp_name, pattern = "ARA Werdh"), "ARA Werdhölzli", wwtp_name))
+
+if (!identical(data_eawag6, newest_data_cloud6)) {
+  message("Copying new RESP6 data from cloud folder to local folder")
+  date_cloudfile_modified6 <- format(as.Date(file.info(eawag_cloud_filepath6)$mtime)+1, "%y-%m-%d")
+  file.copy(eawag_cloud_filepath6, file.path("data/raw_data/eawag_data",paste0("FilteredData-RESP6_",date_cloudfile_modified,".csv")))
+  data_eawag6 <- newest_data_cloud6
+} else {
+  message("No new RESP6 data in cloud folder found.")
+}
+
+data_eawag6 <- data_eawag6 |> 
+  distinct() |> 
+  rename(
+    replicate = Replicate,
+    dilution = Dilution,
+    target = Target,
+    spiked = Spiked,
+    file_name = Filename,
+    run_date = RunDate,
+    sample_type = SampleType,
+    TotalDroplets = TotDroplets
+    )
+
+data_eawag6 <- data_eawag6 %>%
+  mutate(gc.per.mLWW. = gc_per_Lww/1000, target = stringr::str_replace(target, "-", ".")) %>%
+  select(sample_date, wwtp, target, ChamberID, replicate, gc.per.mLWW., file_name, sample_type, flow) %>%
+  filter(!(wwtp=="CDA Lugano" & sample_date=="2023-08-30" & flow ==26669.00)) |> # temporary workaround: filter out smaller value
+  filter(!(wwtp=="CDA Lugano" & sample_date=="2023-08-30" & flow ==19761.00)) |> # temporary workaround: filter out smaller value
+  pivot_wider(id_cols = -c(target, gc.per.mLWW.),
+              names_from = target, values_from = gc.per.mLWW., names_glue = "{target}_.gc.mLWW.")
+
+## Combine data
+data_eawag <- bind_rows(list(
+  resp4 = data_eawag4 |> select(
+    assay = target,
+    sample_date, wwtp, ChamberID, replicate,
+    TotalDroplets,
+    file_name, sample_type,
+    IAV.M_.gc.mLWW.,
+    IBV.M_.gc.mLWW.,
+    SARS.N1_.gc.mLWW.,
+    SARS.N2_.gc.mLWW.
+  ),
+  resp6 = data_eawag6 |> 
+    mutate(assay = "resp6") |> select(
+    assay,
+    sample_date, wwtp, ChamberID, replicate,
+    file_name, sample_type,
+    IAV.M_.gc.mLWW.,
+    IBV.M_.gc.mLWW.,
+    SARS.N1_.gc.mLWW.,
+    SARS.N2_.gc.mLWW.
+  )
+))
+
+# Flow data
 print(paste("Loading flow data from online EAWAG sensors"))
 flow_data_gr <- read.table(
   "https://sensors-eawag.ch/sars/__data__/processed_normed_data_chur_v2.csv",
@@ -92,7 +159,7 @@ control_data_long_eawag <- control_data_eawag %>%
   ))
 
 clean_data_eawag <- data_eawag %>%
-  filter(TotalDroplets >= 15000, sample_type == "ww") %>%
+  filter(TotalDroplets >= 15000 | is.na(TotalDroplets), sample_type == "ww") %>%
   rename(IAV_gc_per_mL_WW = IAV.M_.gc.mLWW.) %>%
   rename(IBV_gc_per_mL_WW = IBV.M_.gc.mLWW.) %>%
   rename(SARS2_N1_gc_per_mL_WW = SARS.N1_.gc.mLWW.) %>%
@@ -177,7 +244,7 @@ clean_data_long_eawag <- rbind(
     sample_date,
     wwtp,
     measuring_period,
-    target,
+    assay,
     IAV_gc_per_mL_WW,
     IBV_gc_per_mL_WW,
     SARS2_N1_gc_per_mL_WW,
@@ -194,7 +261,7 @@ clean_data_long_eawag <- rbind(
     names_to = "measurement_type")
 
 clean_data_long_means_eawag <- clean_data_long_eawag %>%
-  group_by(sample_date, measuring_period, wwtp, target, measurement_type) %>%
+  group_by(assay, sample_date, measuring_period, wwtp, measurement_type) %>%
   summarize(
     mean = mean(value),
     min = min(value),
@@ -204,17 +271,17 @@ clean_data_long_means_eawag <- clean_data_long_eawag %>%
 
 # Plot controls (manually inspect)
 try({
-  ggplot(data = control_data_long_eawag, aes(x = target, y = value)) +
-  geom_boxplot(aes(color = target)) +
+  ggplot(data = control_data_long_eawag, aes(x = assay, y = value)) +
+  geom_boxplot(aes(color = assay)) +
   facet_grid(name ~ sample_type, scales = "free_y") +
-  labs(x = "Target", y = "Measurement (genome copies per mL wastewater)")
+  labs(x = "Assay", y = "Measurement (genome copies per mL wastewater)")
 
 ggsave("figures/all_controls_eawag.png", width = 7, height = 7, units = "in")
 })
 
 # Plot all data
 ggplot(data = clean_data_long_means_eawag,
-       aes(x = as.Date(sample_date), y = mean, color = target)) +
+       aes(x = as.Date(sample_date), y = mean, color = assay)) +
   geom_point() +
   geom_errorbar(aes(ymin = min, ymax = max), width = 5) +
   facet_grid(measurement_type ~ wwtp + measuring_period, scales = "free") +
@@ -226,8 +293,8 @@ ggsave("figures/all_data_eawag.png", width = 9, height = 9, units = "in")
 
 # Select data for influenza analysis
 clean_data_long_eawag_for_analysis <- clean_data_long_eawag %>%
-  filter(!(target %in% c("sn1flu", "sn1(2x)flu"))) %>%  # remove data from earlier assays with different primer and/or probe concentrations
-  filter(!(measurement_type %in% c("IBV_gc_per_mL_WW", "IBV_gc_per_day") & target == "iabv")) %>%  # remove IABV assay data for IBV due to poor separation between positive and negative droplets in controls
+  filter(!(assay %in% c("sn1flu", "sn1(2x)flu"))) %>%  # remove data from earlier assays with different primer and/or probe concentrations
+  filter(!(measurement_type %in% c("IBV_gc_per_mL_WW", "IBV_gc_per_day") & assay == "iabv")) %>%  # remove IABV assay data for IBV due to poor separation between positive and negative droplets in controls
   filter(!(sample_date <= as.Date("2021-12-07") & measurement_type %in% c("IBV_gc_per_mL_WW", "IBV_gc_per_day") & wwtp == "ARA Werdhölzli")) %>%  # single non-zero measurement followed by many 0 measurements for IBV in Zurich cause estimateR bug where minimum incidence not met, this is a crude solution to remove early measurements
   filter(!(measurement_type %in% c("SARS2_N1_gc_per_mL_WW", "SARS2_N2_gc_per_mL_WW")))  # don't analyze SARS-CoV-2
 
@@ -240,9 +307,19 @@ clean_data_long_means_eawag_for_analysis <- clean_data_long_eawag_for_analysis %
     min = min(value),
     max = max(value),
     n_measurements = n(),
+    assays = paste(unique(assay), collapse = "+"),
     .groups = "drop")
 
 # Plot data used in analysis
+ggplot(data = clean_data_long_means_eawag_for_analysis,
+       aes(x = as.Date(sample_date), y = mean)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = min, ymax = max), width = 5) +
+  facet_grid(measurement_type ~ wwtp, scales = "free") +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b\n%Y") +
+  theme(legend.position = "bottom") +
+  labs(x = element_blank(), y = "Mean and min/max measurement across analyzed replicates")
+
 ggplot(data = clean_data_long_means_eawag_for_analysis,
        aes(x = as.Date(sample_date), y = mean)) +
   geom_point() +
